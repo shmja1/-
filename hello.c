a3a50ebb9bd7b33685d8d3c8ce56b1d21c804c6c
Charlie Morningstar <164516294+shmja1@users.noreply.github.com>
2024-11-22 12:16:56 +0800

    更新 hello.c

--- hello.c
+++ hello.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+int up(int* ages ,int length){
+    for(int i ; int i < length-1; i++){
+        for(inr j; int j < length-1-i; j++){
+            if
+
+        }
+    }
+
+}
 int main(){
-    printf("hellow");
+    int ages[]={12,28,368,27694,25687,36712};
+    
+    
 }
